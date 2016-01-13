f44659b460da25610afbf760c459f6a989391ce3
Mike Scott <mike.scott@sqs.com>
2016-01-13 00:10:27 +0000

    tidy up packaging, add eclipse launcher

--- ubuntu-desktop/start-ubuntu.sh
+++ ubuntu-desktop/start-ubuntu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker run -it --rm -p 5901:5901 -e USER=root ubuntu-desktop \
+docker run -it --rm -p 5901:5901 -e USER=root \
+    -v $PWD/data:/data \
+    ubuntu-desktop \
     bash -c "vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log"
--- ubuntu-java-dev/start-java-dev.sh
+++ ubuntu-java-dev/start-java-dev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker run -it --rm -p 5901:5901 -e USER=root ubuntu-java-dev \
+docker run -it --rm -p 5901:5901 -e USER=root \
+    -v $PWD/projects:/data/projects \
+    ubuntu-java-dev \
     bash -c "vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log"
